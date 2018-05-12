fe24200f59fbbb166a1b46eec2c6c0cb79357717
Mike Golubitsky <mike617@gmail.com>
2018-05-11 21:58:04 -0400

    construct binary tree from list

--- src/data_structures/binary_tree.py
+++ src/data_structures/binary_tree.py
@@ -1,15 +1,68 @@
+from src.data_structures.queue import Queue
+
+
 class BinaryTreeNode:
 
+    __unassigned = -1
+
     @classmethod
-    def __construct_from_list(self, list):
-        raise NotImplementedError
+    def __node_for_construction(self, data):
+        if(data is None):
+            return data
+
+        node = BinaryTreeNode(data)
+        node.left = self.__unassigned
+        node.right = self.__unassigned
+
+        return node
 
     @classmethod
     def construct_from_list(self, list):
         """
-        [4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]
+            Creates a binary tree from a level-ordered list of values.
+            Empty children for existing nodes should be specified with
+            None.
+
+            e.g. [4,7,3,None,None,1] yields a tree of shape:
+                 4
+                / \
+               7   3
+                  /
+                 1
         """
-        raise NotImplementedError
+        queue = Queue()
+        root = self.__node_for_construction(list[0])
+        current = root
+
+        for data in list[1:]:
+            new = self.__node_for_construction(data)
+            if new is not None:
+                queue.enqueue(new)
+
+            if(current.left == self.__unassigned):
+                current.left = new
+            elif(current.right == self.__unassigned):
+                current.right = new
+            else:  # current node is full
+                current = queue.dequeue()
+                current.left = new
+
+        # clean up (ensure unassigned values are represented as None)
+        queue = Queue()
+        queue.enqueue(root)
+        while(not queue.is_empty()):
+            current = queue.dequeue()
+            if(current.left == self.__unassigned):
+                current.left = None
+            elif(current.left):
+                queue.enqueue(current.left)
+
+            if(current.right == self.__unassigned):
+                current.right = None
+            elif(current.right):
+                queue.enqueue(current.right)
+
+        return root
 
     def __init__(self, data):
         self.data = data
@@ -60,3 +113,18 @@ class BinaryTreeNode:
         self.right and self.right.traverse_post_order(
             traverse_step_func, *args)
         traverse_step_func(self.data, *args)
+
+    def traverse_level_order(self):
+        queue = Queue()
+        queue.enqueue(self)
+
+        arr = []
+
+        while(not queue.is_empty()):
+            current = queue.dequeue()
+            arr.append(current.data)
+            for child in [current.left, current.right]:
+                if(child):
+                    queue.enqueue(child)
+
+        return arr
--- tests/test_data_structures/test_binary_tree.py
+++ tests/test_data_structures/test_binary_tree.py
@@ -23,6 +23,10 @@ def test_traverse_post_order():
     assert sut.traverse_post_order() == [4, 2, 5, 3, 1]
 
 
+def test_traverse_level_order():
+    assert sut.traverse_level_order() == [1, 2, 3, 4, 5]
+
+
 def _setup_traverse_func_test():
     d = {
         'total': 0
@@ -66,36 +70,29 @@ def test_traverse_post_order_accepts_func_and_args():
     assert external_data['total'] == 20
 
 
-# WIP
+def test_construct_from_list():
+    # arrange
+    # e.g.
+    #    4
+    #  7   3
+    #     1
+    input = [4, 7, 3, None, None, 1]
 
-# def test_construct_from_list():
-#     # arrange
-#     # e.g.
-#     #    4
-#     #  7   3
-#     #     1
-#     input = [4, 7, 3, None, None, 1]
-#     # act
-#     sut = BinaryTreeNode.construct_from_list(input)
-#     actual = []
+    sut = BinaryTreeNode.construct_from_list(input)
 
-#     def func(x): return actual.append(x)
-
-#     sut.traverse_post_order(func)
-
-#     # assert
-#     expected = list(filter(lambda x: x is not None, input))
-#     assert actual == expected
+    # act / assert
+    assert sut.traverse_level_order() == [4, 7, 3, 1]
 
 
-# def test_construct_from_list_complex():
-#     # arrange
-#     input = [4, -7, -3, None, None, -9, -3, 9, -7, -4, None, 6, None, -6, -6,
-#              None, None, 0, 6, 5, None, 9, None, None, -1, -4, None, None, None, -2]
+def test_construct_from_list_complex():
+    # arrange
+    input = [4, -7, -3, None, None, -9, -3, 9, -7, -4, None, 6, None, -6, -6,
+             None, None, 0, 6, 5, None, 9, None, None, -1, -4, None, None, None, -2]
 
-#     # act
-#     actual = BinaryTreeNode.construct_from_list(input)
+    # act
+    sut = BinaryTreeNode.construct_from_list(input)
+    actual = sut.traverse_level_order()
 
-#     # assert
-#     expected = list(filter(lambda x: x is not None, input))
-#     assert actual == expected
+    # assert
+    expected = list(filter(lambda x: x is not None, input))
+    assert actual == expected
